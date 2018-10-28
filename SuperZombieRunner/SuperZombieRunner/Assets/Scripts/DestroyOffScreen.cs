a72f7a1f523c7182cf38d914367f52fb7cace39e
truongaaron <plainaaron@gmail.com>
2018-10-27 19:22:36 -0700

    Completed up to lesson 4-2 (Start 4-3)

---
+++ SuperZombieRunner/SuperZombieRunner/Assets/Scripts/DestroyOffScreen.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOffScreen : MonoBehaviour {
+
+    public float offset = 16f;
+
+    private bool offscreen;
+    private float offScreenX = 0;
+    private Rigidbody2D body2d;
+
+    void Awake()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        offScreenX = (Screen.width / PixelPerfectCamera.pixelsToUnits) / 2 + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var posX = transform.position.x;
+        var dirX = body2d.velocity.x;
+
+        if(Mathf.Abs(posX) > offScreenX)
+        {
+            if(dirX < 0 && posX < -offScreenX) {
+                offscreen = true;
+            } else if(dirX > 0 && posX > offScreenX) {
+                offscreen = true;
+            }            
+
+        } else {
+            offscreen = false;
+        }
+
+        if (offscreen)
+        {
+            OnOutOfBounds();
+        }
+	}
+
+    public void OnOutOfBounds()
+    {
+        offscreen = false;
+        GameObjectUtil.Destroy(gameObject);
+    }
+}
---
+++ SuperZombieRunner/SuperZombieRunner/Assets/Scripts/GameObjectUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectUtil {
+
+    public static GameObject Instantiate(GameObject prefab, Vector3 pos){
+        GameObject instance = null;
+
+        instance = GameObject.Instantiate(prefab);
+        instance.transform.position = pos;
+
+        return instance;
+    }
+
+    public static void Destroy(GameObject gameObject)
+    {
+
+        var recycleGameObject = gameObject.GetComponent<RecycleGameObject>();
+
+        if (recycleGameObject != null) 
+        {
+            recycleGameObject.Shutdown();
+        } else {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
