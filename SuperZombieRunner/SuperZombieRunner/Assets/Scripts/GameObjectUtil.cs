3fe468c39e8a292b72d69a9727d96b3f51d446e9
truongaaron <plainaaron@gmail.com>
2018-10-28 09:37:21 -0700

    Finished Section 4, Start Section 5

--- SuperZombieRunner/SuperZombieRunner/Assets/Scripts/GameObjectUtil.cs
+++ SuperZombieRunner/SuperZombieRunner/Assets/Scripts/GameObjectUtil.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class GameObjectUtil {
 
+    private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>();
+
     public static GameObject Instantiate(GameObject prefab, Vector3 pos){
         GameObject instance = null;
 
-        instance = GameObject.Instantiate(prefab);
-        instance.transform.position = pos;
+        var recycledScript = prefab.GetComponent<RecycleGameObject>();
+        if (recycledScript != null) {
+            var pool = GetObjectPool(recycledScript);
+            instance = pool.NextObject(pos).gameObject;
+        }
+        else {
+
+            instance = GameObject.Instantiate(prefab);
+            instance.transform.position = pos;
+        }
 
         return instance;
     }
@@ -18,11 +28,26 @@ public class GameObjectUtil {
 
         var recycleGameObject = gameObject.GetComponent<RecycleGameObject>();
 
-        if (recycleGameObject != null) 
-        {
+        if (recycleGameObject != null) {
             recycleGameObject.Shutdown();
         } else {
             GameObject.Destroy(gameObject);
         }
+    }
+
+    private static ObjectPool GetObjectPool(RecycleGameObject reference)
+    {
+        ObjectPool pool = null;
+
+        if (pools.ContainsKey(reference)) {
+            pool = pools[reference];
+        } else {
+            var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
+            pool = poolContainer.AddComponent<ObjectPool>();
+            pool.prefab = reference;
+            pools.Add(reference, pool);
+        }
+
+        return pool;
     }
 }
--- SuperZombieRunner/SuperZombieRunner/Assets/Scripts/RecycleGameObject.cs
+++ SuperZombieRunner/SuperZombieRunner/Assets/Scripts/RecycleGameObject.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IRecycle {
+
+    void Restart();
+    void Shutdown();
+
+}
+
 public class RecycleGameObject : MonoBehaviour {
-    
-    public void Restart()
-    {
-        gameObject.SetActive(true);
+
+    private List<IRecycle> recycleComponents;
+
+    void Awake(){
+
+        var components = GetComponents<MonoBehaviour>();
+        recycleComponents = new List<IRecycle>();
+        foreach(var component in components)
+        {
+            if(component is IRecycle)
+            {
+                recycleComponents.Add(component as IRecycle);
+            }
+        }
     }
 
-    public void Shutdown()
-    {
+    public void Restart() {
+        gameObject.SetActive(true);
+
+        foreach(var component in recycleComponents)
+        {
+            component.Restart();
+        }
+    }
+
+    public void Shutdown() {
         gameObject.SetActive(false);
+
+        foreach (var component in recycleComponents)
+        {
+            component.Shutdown();
+        }
     }
 }
